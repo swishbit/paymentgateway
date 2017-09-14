644a427c8d72d4795edcf092bcb540022632f7fa
Carl Barrdahl <carlbarrdahl@gmail.com>
2017-09-14 10:43:39 +0200

    Setup tests

---
+++ test/index.test.js
@@ -0,0 +1,31 @@
+test("users should be able to make a swish payment and receive bitcoins to provided address", () => {
+  const actual = true
+  const expected = false
+  expect(actual).toBeDefined(expected)
+})
+
+test("it should verify bitcoin address", () => {
+
+})
+
+test("it should log the requested transaction", () => {
+
+})
+
+test("it should ensure sufficient balance", () => {
+
+})
+
+test("it should convert SEK/BTC", () => {
+
+})
+
+test("it should calculate fee", () => {
+
+})
+
+test("it should transfer to address", () => {
+
+  // return SEK to user on error
+})
+
